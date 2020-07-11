ec0b1f7d172dc550bf5c5aa1b868b02cf26928cc
grahovsky <grahovsky@gmail.com>
2020-07-11 22:14:15 +0300

    numbers

---
+++ basics/numbers/numbers.groovy
@@ -0,0 +1,22 @@
+1.getClass().getName()
+//java.lang.Integer
+
+1132412341234.getClass().getName()
+//java.lang.Long
+
+int x = 1
+x.class
+//class java.lang.Integer
+
+def y = 2
+y.class
+//class java.lang.Integer
+
+5.5.class
+//class java.math.BigDecimal
+
+def z = 5.5d
+z.class
+//class java.lang.Double
+
+
