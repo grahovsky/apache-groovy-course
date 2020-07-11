0477a9287dda0f4e567de19be674443a95d1b70d
grahovsky <grahovsky@gmail.com>
2020-07-11 17:55:00 +0300

    comments

---
+++ basics/comments/shebang.groovy
@@ -0,0 +1,19 @@
+#!/usr/bin/env groovy
+println "Hello from the shebang line"
+
+// this is a single line comment
+
+/*
+multiline
+comment
+example
+ */
+
+/**
+ *  This is a person class
+ *  read this documentations
+ *
+ */
+class Person {
+
+}
