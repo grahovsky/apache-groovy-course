8a4eefd095ec07e669c8215447abeff988f25052
grahovsky <grahovsky@gmail.com>
2020-07-13 00:00:10 +0300

    closures basisc

---
+++ closures/Basics.groovy
@@ -0,0 +1,42 @@
+def c = { }
+
+println c.class.name
+println c instanceof Closure
+
+def sayHello = { name ->
+    println "Hello, $name"
+}
+
+sayHello('Dan')
+
+List nums = [1,4,7,4,30,2]
+nums.each { num ->
+    println num
+}
+
+nums.each { print it }
+//1474302
+
+// closures are objects & last param
+
+def timesTen(num,closure) {
+    closure(num * 10)
+}
+
+timesTen(10, { println it })
+// if closures is last arg, can use this syntax:
+timesTen(2) { 
+    println it 
+}
+
+10.times { 
+    println it
+}
+
+import java.util.Random
+
+Random rand = new Random()
+
+3.times {
+    println rand.nextInt()
+}
